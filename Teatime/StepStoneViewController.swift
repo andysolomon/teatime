c77d9c0f89050503e83ee566c953d04d579d8dd0
Andrew Solomon <andrewsolomon.edu@gmail.com>
2015-06-16 21:47:30 -0400

    Added very basic timer functionality to steps page

--- Teatime/StepStoneViewController.swift
+++ Teatime/StepStoneViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class StepStoneViewController: UIViewController {
+    
+    var timerCount = 0
+    var timerRunning = false
+    var timer = NSTimer()
 
     @IBOutlet var stepNameLabel: UILabel!
+    @IBOutlet var stepTimerLabel: UILabel!
+    
+    func counting(){
+        timerCount += 1
+        stepTimerLabel.text = "\(timerCount)"
+    }
+    func stopTimer() {
+        if timerRunning == true {
+            timer.invalidate()
+            timerRunning == false
+        }
+    }
+    @IBAction func toggleTimerStart(sender: UIButton) {
+        if timerRunning == false {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("counting"), userInfo: nil, repeats: true)
+        timerRunning = true
+        } else {
+            timer.invalidate()
+            timerRunning = false
+        }
+
+    }
+    @IBAction func restartTimer(sender: UIButton) {
+        timerCount = 0
+        stepTimerLabel.text = "0"
+        stopTimer()
+    }
 
 }
