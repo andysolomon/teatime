0e8163d1f26dbbc4c97f9f17996c1fa46b235126
Andrew Solomon <andrewsolomon.edu@gmail.com>
2015-06-22 18:12:48 -0400

    Add AppDelegate back to root

--- Teatime/StepStoneViewController.swift
+++ Teatime/StepStoneViewController.swift
@@ -12,10 +12,27 @@ class StepStoneViewController: UIViewController {
 
     @IBOutlet var stepNameLabel: UILabel!
     @IBOutlet var stepTimerLabel: UILabel!
+    
     var tea: JSON!
     var timerCount =  200
     var timerRunning = false
     var timer = NSTimer()
+    
+    //// Color Declarations
+    let timerCircle = UIColor(red: 0.020, green: 0.024, blue: 0.031, alpha: 1.000)
+    
+    //// Oval Drawing
+    //var ovalPath = UIBezierPath(ovalInRect: CGRectMake(76, 19, 150, 150))
+    //timerCircle.setStroke()
+    //ovalPath.lineWidth = 10
+    //ovalPath.stroke()
+    
+    let path = UIBezierPath()
+    path.moveToPoint(CGPoint(80, 50))
+    path.addLineToPoint(CGPoint(89, 150))
+    path.addLineToPoint(CGPoint(10, 150))
+    
+    path.linewidth = 10.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
