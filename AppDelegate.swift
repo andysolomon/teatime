555fec70b85020e8ce24b03c99c884fc4135dfc6
Jimmy McDermott <mcdappdev@gmail.com>
2015-06-23 12:41:21 -0500

    fixed app delegate

---
+++ AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Teatime
+//
+//  Created by Jim McDermott on 6/23/15.
+//  Copyright (c) 2015 teateam. All rights reserved.
+//
+
+import UIKit
+
+class AppDelegate: NSObject {
+   
+}
